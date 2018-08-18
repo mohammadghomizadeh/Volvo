a315ee76175f77f8d05cacb106362f525e1b8888
Mohammad Ghomizadeh <m.ghomizadeh96@gmail.com>
2018-08-18 15:06:27 +0430

    write article edit 90%

--- adminpanel/pages/article-edit.php
+++ adminpanel/pages/article-edit.php
@@ -1,27 +1,27 @@
 <?php
+
 include('config/db.php');
 $id = $_GET['id'];
 $result = $db->select("content","`id` = $id");
-
-foreach($result as $row ){
+foreach($result as $rowarticle ){
 ?>
 <div class="box-header with-border">
               <h3 class="box-title">ویرایش مطلب</h3>
             </div>
             <hr>
-      <form method="POST" action="index.php?page=article-edit&id=insert" enctype="multipart/form-data">
+      <form method="POST" action="index.php?page=article-edit&action=edit&id=<?php echo $rowarticle['id'];  ?>" enctype="multipart/form-data">
             <div class="form-group">
                   <label>عنوان مطلب</label>
-                  <input type="text" class="form-control" placeholder="متن" name="articletitle" id="articletitle" value="<?php echo $row['title'];  ?>">
+                  <input type="text" class="form-control" placeholder="متن" name="articletitle" id="articletitle" value="<?php echo $rowarticle['title'];  ?>">
              </div>
               </div>
-
+              
               <div class="form-group">
               <label>دسته بندی مقالات</label>
                   <div class="checkbox">
                   <?php
                  
-                      $result = $db->select("content","`type` = 'category' AND `parent` = 100 ");
+                      $result = $db->select("content","`type` = 'category' AND `parent` = 100");
                       foreach($result as $row)
                       {
                         ?>
@@ -43,21 +43,24 @@ foreach($result as $row ){
                 <div class="form-group">
                 <label>متن</label>
                 <textarea id="editor1" name="editor1" rows="10" cols="80">
-                <?php echo $row['description'];?>
+                <?php echo $rowarticle['description'];?>
                 </textarea>
                 </div>
                 <div class="form-group">
 
                   <label for="articleimage">تصویر مطلب</label>
 
-                  <input type="file"  name="articleimage" id="articleimage" required >
+                  <input type="file"  name="articleimage" id="articleimage">
                   <p id="error1" style="display:none; color:#FF0000;">
                   خطا
                   </p>
                   <p id="error2" style="display:none; color:#FF0000;">
                   حجم بالاست
                   </p>
-                  <img scr="uploads/<?php echo $row['image'];?>" width="200" height="200" />
+                  <div class="text-left">
+                    <label for="articleimage">تصویر انتخاب شده </label>
+                    <img src="uploads/<?php echo $rowarticle['image'];?>" width="200" height="200" />
+                    </div>
                   <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>
 
                         <script>
@@ -102,5 +105,26 @@ foreach($result as $row ){
               
 </form>
 <?php
+}
+if(isset($_GET['action']))
+{
+    if($_GET['action'] == "edit")
+    {
+      include('config/db.php');
+      $art_id = $_GET['action'];
+      $arttitle = $_POST['articletitle'];
+      $artdescript = $_POST['editor1'];
+      $artcat = $_POST['category'];
+      $artimg = $_POST['articleimage'];
+      $r = $db->update("content", "`title`='$arttitle' ,`description` = '$artdescript' ,`image` = '$artimg' ,`parent` = '$artcat'","`id` = '$art_id '");
+      if($r)
+      {
+?>
+                      <script>
+                      window.location.replace("index.php?page=articles&action=list&id=0");
+                      </script>
+<?php
+      }
+    }
 }
 ?>
